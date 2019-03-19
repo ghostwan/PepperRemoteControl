265ec3d72ee8c98ed29599876317d586f3eb893d
Ghostwan <ghostwan@gmail.com>
2019-03-19 07:57:52 +0100

    Gradle Lib config, add libqi-java jar, androidx, tests skeleton,

---
+++ pepperremote/src/test/java/com/ghostwan/pepperremotecontrol/robot/RobotTest.kt
@@ -0,0 +1,19 @@
+package com.ghostwan.pepperremotecontrol.robot
+
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+class RobotTest {
+
+    @Before
+    fun setup() {
+//        MockKAnnotations.init(this, relaxUnitFun = true)
+    }
+
+    @Test
+    fun addition_isCorrect() {
+        Assert.assertEquals(4, 2 + 2)
+    }
+}
+
